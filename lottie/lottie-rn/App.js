3d7ec473634c2565dc7be001a02a07fadd5f0fdf
FelipeCorerato <fcorerato@gmail.com>
2019-11-05 08:53:54 -0200

    Novo projeto: Sequelize
    :blowfish:

--- lottie/lottie-rn/App.js
+++ lottie/lottie-rn/App.js
@@ -1,110 +1,167 @@
 import React from 'react';
-import { Button, StyleSheet, SafeAreaView, View, Text, Animated, Easing } from 'react-native';
+import { 
+  StyleSheet, 
+  StatusBar,
+  SafeAreaView, 
+  View, 
+  Text, 
+  Animated, 
+  Easing,
+  Dimensions,
+} from 'react-native';
+import * as Font from 'expo-font';
 import LottieView from "lottie-react-native";
+
+const dimensions = Dimensions.get('window');
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.animatedValue = new Animated.Value(0)
+
+    this.animatedDoorValue = new Animated.Value(0);
+    this.animatedColorValue = new Animated.Value(0);
+
+    this.state = {
+      fontLoaded: false,
+      unmont: false
+    }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    // Carrega a fonte do App
+    await Font.loadAsync({
+      'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+
+    // Inicia a animacao Lottie
     this.animation.play();
-    this.animate();
+
+    // Inicia a animacao da porta
+    this.closeDoorsAnimation();
+  }
+  
+  closeDoorsAnimation() {
+    this.animatedDoorValue.setValue(0);
+
+    Animated.timing(this.animatedDoorValue, {
+      toValue: 1,
+      duration: 3000,
+      ezasing: Easing.linear
+    }).start(() => this.opacityAnimation());
   }
 
-  resetAnimation = () => {
-    this.animation.reset();
-    this.animation.play();
-  };
-
-  animate() {
-    this.animatedValue.setValue(0);
-
-    Animated.timing(
-      this.animatedValue, {
-        toValue: 1,
-        duration: 2000,
-        ezasing: Easing.linear
-      }
-    ).start(() => this.animate())
+  opacityAnimation() {
+    Animated.timing(this.animatedColorValue, { 
+      toValue: 150,
+      duration: 3000,
+      ezasing: Easing.linear
+    }).start(() => this.openDoorsAnimation());
   }
 
-  openDoorAnimation() {
-    this.animatedValue.setValue(0);
+  openDoorsAnimation() {
+    this.setState({ unmont: true });
+    this.animatedDoorValue.setValue(1);
 
-    //Animated.timing(
-
-    //).start(() => this.openDoorAnimation());
+    Animated.timing(this.animatedDoorValue, {
+      toValue: 0,
+      duration: 3000,
+      ezasing: Easing.linear
+    }).start();
   }
 
   render() {
-    const movingMargin = this.animatedValue.interpolate({
-      inputRange: [0, 0.5, 1],
-      outputRange: [0, 200, 0]
-    })
+    const movingMargin = this.animatedDoorValue.interpolate({
+      inputRange: [0, 0.5],
+      outputRange: [0, dimensions.width/2],
+      extrapolate: 'clamp'
+    });
+
+    var color = this.animatedColorValue.interpolate({
+      inputRange: [0, 150],
+      outputRange: ['rgba(69, 70, 71, 0.4)', 'rgba(69, 70, 71, 1)']
+    });
 
     return (
-      <SafeAreaView style={styles.animationContainer}>
-        <Animated.View 
-          style={{ 
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: '-50%',
-            zIndex: 2,
-            width: '50%', 
-            backgroundColor: 'rgba(69, 70, 71, 0.7)', 
-            borderRightWidth: 20,
-            borderColor: '#545758',
-            marginLeft: movingMargin 
-          }} 
-        />
-        {/*<Animated.View 
-          style={{ 
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            right: 0,
-            zIndex: 2,
-            width: '50%', 
-            backgroundColor: 'rgba(69, 70, 71, 0.7)', 
-            borderLeftWidth: 20,
-            borderColor: '#545758',
-            marginLeft: movingMargin 
-          }} 
-        />*/}
+      <>
+        <StatusBar barStyle='light-content' />
 
-        <Text>R.E.I</Text>
-        <LottieView
-          ref={animation => {
-            this.animation = animation;
-          }}
-          style={{
-            width: 400,
-            height: 400,
-            backgroundColor: '#fff',
-          }}
-          source={require('./assets/bus.json')}
-        />
+        <Animated.View style={[styles.busDoors, { 
+          left: '-50%', backgroundColor: color, borderRightWidth: 20, marginLeft: movingMargin 
+        }]} />
 
-        <View style={styles.buttonContainer} />
-      </SafeAreaView>
+        <Animated.View style={[styles.busDoors, { 
+          right: '-50%', backgroundColor: color, borderLeftWidth: 20, marginRight: movingMargin 
+        }]} />
+
+        { this.state.unmont ? null : (
+          <>
+            <SafeAreaView style={{ flex: 0, backgroundColor: '#fff' }} /> 
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#8AC348' }}>
+              <View style={styles.animationContainer}>
+                { this.state.fontLoaded && (
+                  <Text style={styles.title}>R E I</Text>
+                )}
+                
+                <LottieView
+                  style={styles.lottie}
+                  ref={animation => this.animation = animation}
+                  source={require('./assets/bus.json')}
+                />
+
+                <View style={styles.buttonContainer} />
+              </View>
+            </SafeAreaView>
+          </>
+        )}          
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
   animationContainer: {
-    backgroundColor: 'black',
+    flex: 1,
+
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
+    
+    backgroundColor: '#fff'
   },
+
+  lottie: {
+    width: 400, 
+    height: 400, 
+    
+    backgroundColor: '#fff'
+  },
+
   buttonContainer: {
-    backgroundColor: '#8AC348',
+    flex: 2,
+
     width: '100%',
     paddingTop: 20,
-    flex: 2
+    
+    backgroundColor: '#8AC348'
   },
+
+  title: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 60,
+
+    textAlign: 'center',
+    marginTop: 10,
+
+    color: '#515151'
+  },
+
+  busDoors: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    zIndex: 2,
+    width: '50%', 
+    borderColor: '#545758',
+  }
 });
---
+++ sqlnode/src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+    database: 'sqlnode',   
+    username: 'fxiiwfol',
+    password: 'B15_O5yFJgn2NobFhpeGXawiqMvyNtoG',
+    host: 'motty.db.elephantsql.com',
+    dialect: 'postgres',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
+// module.exports = {
+//     url: 'postgres://fxiiwfol:B15_O5yFJgn2NobFhpeGXawiqMvyNtoG@motty.db.elephantsql.com:5432/fxiiwfol'
+// }
